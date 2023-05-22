d3be6e3f96f5681e576b4c28811c0309a6a8a146
Siddharth Asthana <siddharthasthana1320@gmail.com>
2023-05-22 19:48:23 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Alert from "./components/Alert";
-import About from "./components/About";
+// import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -33,24 +33,24 @@ function App() {
     <>
       {/* <Navbar title="TextUtills" aboutText="About TextUtills" /> */}
       {/* <Navbar/> */}
-      <BrowserRouter>
+     {/* <BrowserRouter> */}
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
+          {/* <Routes> 
             <Route path="/about" element={<About mode={mode} />}>
               
-            </Route>
-            <Route path="/" element={ <TextForm
+            </Route> */}
+            {/*<Route path="/" element={ */}<TextForm
                 showalert={showalert}
                 heading="ManupulateText - word counter, Character counter, Remove extra spaces"
                 mode={mode}
-              />}>
+            />}> 
              
-            </Route>
-          </Routes>
+           {/* </Route> */}
+          { /*</Routes> */}
         </div>
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
     </>
   );
 }
